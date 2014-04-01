4418202f7558f618da901fe521812cac02ff874c
blasterbug <tontonposso@gmail.com>
2014-04-01 18:40:03 +0200

    update doc

--- class_alveole.js
+++ class_alveole.js
@@ -4,6 +4,7 @@ var class_alveole =
     [ "Alveole", "class_alveole.html#a24ac02d39a6eca02630f6f2a11d86575", null ],
     [ "Alveole", "class_alveole.html#aa935efb574de27fcf5171e1a631f40c9", null ],
     [ "Alveole", "class_alveole.html#afc611d6ccd9406b36c1326653cc80f1e", null ],
+    [ "~Alveole", "class_alveole.html#ac5b90a69dc1458b981a742e80499aca0", null ],
     [ "getKey", "class_alveole.html#add57358f630300978cfd314a9a392c8b", null ],
     [ "getNext", "class_alveole.html#ae540dba4d60c74ff17e2abc03deb33c8", null ],
     [ "getValue", "class_alveole.html#a63378af984be5bb63fb755010c61002f", null ],
--- search/all_e.js
+++ search/all_e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['_7ealveole',['~Alveole',['../class_alveole.html#ac5b90a69dc1458b981a742e80499aca0',1,'Alveole']]],
   ['_7ebagexception',['~BagException',['../class_tree_exception.html#a40fd9844273948231478fcf70e6f9468',1,'TreeException']]],
   ['_7ehashexception',['~HashException',['../class_hash_exception.html#a44832370e4c1914bafe002a27bf2d3a4',1,'HashException']]],
   ['_7ehashtable',['~Hashtable',['../class_hashtable.html#a3f4057afda4ad0f6b805f900e43b53f6',1,'Hashtable']]],
